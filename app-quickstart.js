34a54be5163f97610ce9fe39e11cabcdfbfed509
clintoncontreras <ClintonC@zoovy.com>
2013-07-08 13:03:09 -0700

    Corrected search page for filtered search issues.

--- app-quickstart.js
+++ app-quickstart.js
@@ -1202,12 +1202,14 @@ app.ext.myRIA.pageTransition($old,$('#'+infoObj.parentID));
 					
 /***TOY WARS***/	$(".filterContainerSearch").hide();
 /***TOY WARS***/	$("#resultsProductListContainer").css({'width':'240px'});
+/***TOY WARS***/	$(".searchFilterResults").css({'width':'240px'});
 					
 //button for turning off preview mode. returns li's to normal state and animates the two 'panes'.
 					$("<button \/>").button().text('close preview').on('click',function(event){
 						app.ext.myRIA.u.revertPageFromPreviewMode($parent);
 /***TOY WARS***/		$(".filterContainerSearch").show();
 /***TOY WARS***/	$("#resultsProductListContainer").css({'width':'745px'});
+/***TOY WARS***/	$(".searchFilterResults").css({'width':'760px'});
 						}).prependTo($buttonBar);
 
 
